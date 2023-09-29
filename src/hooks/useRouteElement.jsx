0eb76ee12b9a1daa6d6760091a40ada8eed47ce1
hoangchuong <lehoangchuong.lk@gmail.com>
2023-09-30 02:20:19 +0700

    Login

--- src/hooks/useRouteElement.jsx
+++ src/hooks/useRouteElement.jsx
@@ -1,11 +1,14 @@
 import { useRoutes } from "react-router-dom";
 import Login from "../page/Login/Login";
+import Home from "../page/Home/Home";
 // import Home from "../page/Home/Home";
 
 export default function useRouteElement() {
   const routeElement = useRoutes([
-    { path: "", element: <Login /> },
-    { path: "/login", element: <Login /> },
+    { path: "/", element: <Home /> },
+    { path: "/about", element: <Login /> },
+
+    { path: "login", element: <Login /> },
   ]);
   return routeElement;
 }
