65321498df759bd9d1063ad4c0f0cd3672eab8d3
hoangchuong <lehoangchuong.lk@gmail.com>
2023-09-30 02:10:35 +0700

    len

--- src/hooks/useRouteElement.jsx
+++ src/hooks/useRouteElement.jsx
@@ -2,10 +2,36 @@ import { useRoutes } from "react-router-dom";
 import Login from "../page/Login/Login";
 // import Home from "../page/Home/Home";
 
+// function ProtectedRoute() {
+//   const isAuthenticated = window.localStorage.getItem("user");
+//   return isAuthenticated ? <Outlet /> : <Navigate to="/login" />;
+// }
+
+function RejectRoute() {
+  const isAuthenticated = window.localStorage.getItem("user");
+  return !isAuthenticated ? <Outlet /> : <Navigate to="/home" />;
+}
+
+// function IsAdmin() {
+//   const data = JSON.parse(localStorage.getItem("user") || "{}");
+//   return data ? <Outlet /> : <Navigate to="/" />;
+// }
+
 export default function useRouteElement() {
   const routeElement = useRoutes([
-    { path: "", element: <Login /> },
-    { path: "/login", element: <Login /> },
+    {
+      path: "/",
+      element: <RejectRoute />,
+      children: [
+        {
+          path: "login",
+          element: <Login />,
+        },
+        
+      ],
+    },
+    // { path: "", element: <Login /> },
+    // { path: "/login", element: <Login /> },
   ]);
   return routeElement;
 }
